22bcd00299e880fd8314e83e123ebdf74a5a3250
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-02-17 07:57:51 +0900

    Create WiFiInput component

---
+++ src/molecules/WiFiInput.svelte
@@ -0,0 +1,29 @@
+<script context="module" lang="ts">
+  export type SubmitEvent = CustomEvent<{ ssid: string; password: string }>;
+</script>
+
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import { Button, TextField } from "smelte";
+
+  const dispatch = createEventDispatcher();
+
+  function submit() {
+    dispatch("submit", {
+      ssid,
+      password,
+    });
+  }
+
+  let clazz = "";
+  export { clazz as class };
+  export let ssid: string;
+  export let password: string;
+  export let button = "Connect";
+</script>
+
+<form class={clazz}>
+  <TextField label="SSID" bind:value={ssid} />
+  <TextField type="password" label="Password" bind:value={password} />
+  <Button class="w-full" on:click={submit}>{button}</Button>
+</form>
