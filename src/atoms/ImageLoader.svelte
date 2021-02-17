f48be790dd3809126e0cd1de03db44985e46c1f7
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-02-17 23:34:17 +0900

    Create ImageLoader component

---
+++ src/atoms/ImageLoader.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let img: HTMLImageElement;
+
+  function onChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    if (target.files.length != 1) {
+      return;
+    }
+    const file = target.files[0];
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      img.src = event.target.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+</script>
+
+<input
+  on:change={onChange}
+  type="file"
+  name="image"
+  accept=".jpg,.jpeg,.png,.bmp,.gif"
+/>
