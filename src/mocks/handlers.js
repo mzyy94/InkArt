134d1b77beeab4467b8b4ef261169afbeaf59e78
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-02-17 09:07:15 +0900

    Add aplist mock data

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,5 +1,23 @@
 import { rest } from "msw";
 
+const enc = [
+  "open",
+  "WEP",
+  "WPA_PSK",
+  "WPA2_PSK",
+  "WPA_WPA2_PSK",
+  "WPA2_ENTERPRISE",
+];
+
+const names = [
+  "Panther",
+  "Tiger",
+  "Leopard",
+  "Snow Leopard",
+  "Lion",
+  "Mountain Lion",
+];
+
 export const handlers = [
   rest.get("/status.json", (_req, res, ctx) => {
     return res(
@@ -8,5 +26,15 @@ export const handlers = [
         status: "ok",
       })
     );
+  }),
+  rest.get("/aplist.json", (_req, res, ctx) => {
+    const list = Array.from({ length: 6 }, (_, i) => {
+      const ssid = names[i];
+      const auth = enc[i];
+      const rssi = -50 - ((Math.random() * 50) | 0);
+      return { ssid, auth, rssi };
+    });
+
+    return res(ctx.delay(2000), ctx.status(200), ctx.json(list));
   }),
 ];
