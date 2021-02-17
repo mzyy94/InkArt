5d0a784cf85edc8217156ebab9eec06d8cd6cdea
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-02-18 00:22:43 +0900

    Add cover drawing mode

--- src/atoms/Canvas.svelte
+++ src/atoms/Canvas.svelte
@@ -9,22 +9,34 @@
       return;
     }
     const ctx = canvas.getContext("2d");
+    let x = 0;
+    let y = 0;
+    let { width, height } = canvas;
+    const imageAspect = img.width / img.height;
+    const canvasAspect = canvas.width / canvas.height;
+    const scaleFactor = imageAspect / canvasAspect;
+
     ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(x, y, width, height);
+
     switch (mode) {
       case "fill": {
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, x, y, width, height);
+        break;
+      }
+      case "cover": {
+        if (scaleFactor > 1) {
+          width = width * scaleFactor;
+          x = (canvas.width - width) / 2;
+        } else {
+          height = height / scaleFactor;
+          y = (canvas.height - height) / 2;
+        }
+
+        ctx.drawImage(img, x, y, width, height);
         break;
       }
       case "fit": {
-        let x = 0;
-        let y = 0;
-        let { width, height } = canvas;
-
-        const imageAspect = img.width / img.height;
-        const canvasAspect = canvas.width / canvas.height;
-        const scaleFactor = imageAspect / canvasAspect;
-
         if (scaleFactor < 1) {
           width = width * scaleFactor;
           x = (canvas.width - width) / 2;
