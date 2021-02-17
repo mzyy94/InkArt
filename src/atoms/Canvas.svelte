22a1bf9e910f392b627d0735d8e3af15881dd3d1
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-02-17 23:41:25 +0900

    Create Canvas component

---
+++ src/atoms/Canvas.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let img: HTMLImageElement;
+  export let mode = "fill";
+
+  let canvas: HTMLCanvasElement;
+
+  img.onload = () => {
+    const ctx = canvas.getContext("2d");
+    if (mode == "fill") {
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+  };
+</script>
+
+<canvas bind:this={canvas} width={800} height={600} />
+
+<style>
+  canvas {
+    background: gray;
+    max-width: 100%;
+  }
+</style>
