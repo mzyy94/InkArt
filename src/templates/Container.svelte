0b76447869e98c8b5bfd82af8195547561e82ad3
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-02-27 05:17:55 +0900

    Add container margin

--- src/templates/Container.svelte
+++ src/templates/Container.svelte
@@ -1,6 +1,6 @@
 <h1 class="text-center md:text-4xl text-2xl">
   <slot name="title">Title</slot>
 </h1>
-<section class="md:w-2/3 sm:w-auto m-auto">
+<section class="md:w-2/3 sm:w-auto m-auto my-3">
   <slot />
 </section>
