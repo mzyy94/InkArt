76733150f1185966f9127ae274a36b6703ada748
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-03-01 21:31:15 +0900

    Create TimeInput component

---
+++ src/atoms/TimeInput.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { TextField } from "smelte";
+
+  export let time: string;
+
+  let inputError = "";
+
+  function inputTime(e: Event) {
+    const input = e.target as HTMLInputElement;
+    const pattern = new RegExp(input.pattern);
+
+    if (pattern.test(input.value)) {
+      inputError = "";
+      time = input.value;
+    } else {
+      inputError = "Invalid time format.";
+    }
+  }
+</script>
+
+<TextField
+  value={time}
+  error={inputError}
+  on:change={inputTime}
+  append="access_time"
+  label="Time"
+  pattern="^\d\d:\d\d:\d\d$"
+/>
