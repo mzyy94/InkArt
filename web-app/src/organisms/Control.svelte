220152a5f042235260a9cd134b6c597b3a325ff9
Yuki MIZUNO <mizuyuu0904@gmail.com>
2022-01-08 21:50:02 +0900

    Add padding preview

--- web-app/src/organisms/Control.svelte
+++ web-app/src/organisms/Control.svelte
@@ -14,6 +14,9 @@
     { value: "upside-down", text: "Upside Down" },
   ];
 
+  let canvas: HTMLCanvasElement;
+  let ctx: CanvasRenderingContext2D;
+
   let orientation: Orientation;
   let paddingTop = 0;
   let paddingLeft = 0;
@@ -49,7 +52,77 @@
       });
   }
 
-  onMount(initSettings);
+  onMount(async () => {
+    ctx = canvas.getContext("2d")!;
+    await initSettings();
+  });
+
+  $: if (ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const portrait = orientation == "portrait" || orientation == "upside-down";
+    const width = 800;
+    const height = 600;
+    const scale = 0.5;
+
+    const rotate = () => {
+      ctx.save();
+      ctx.translate(center.x, center.y);
+      if (orientation == "portrait") {
+        ctx.rotate((-Math.PI / 2) * 1);
+      } else if (orientation == "landscape-right") {
+        ctx.rotate((-Math.PI / 2) * 2);
+      } else if (orientation == "upside-down") {
+        ctx.rotate((-Math.PI / 2) * 3);
+      }
+      ctx.scale(scale, scale);
+    };
+
+    const center = {
+      x: canvas.width / 2,
+      y: canvas.height / 2,
+    };
+
+    rotate();
+    // PCB
+    ctx.fillStyle = "#009";
+    ctx.fillRect(
+      -(width + 20) / 2,
+      -(height + 20) / 2,
+      width + 20,
+      height + 60
+    );
+
+    // Touch Pads
+    ctx.strokeStyle = "#fff";
+    ctx.lineWidth = 4;
+    for (let i = 0; i < 3; i++) {
+      ctx.strokeRect(-200 + 160 * i, height / 2 + 8, 60, 30);
+    }
+
+    // Panel
+    ctx.fillStyle = "#888";
+    ctx.fillRect(-width / 2, -height / 2, width, height);
+    ctx.restore();
+
+    ctx.save();
+    ctx.translate(center.x, center.y);
+    ctx.scale(scale, scale);
+    ctx.fillStyle = "#aaa";
+    ctx.fillRect(
+      -((portrait ? height : width) / 2 - paddingLeft),
+      -((portrait ? width : height) / 2 - paddingTop),
+      (portrait ? height : width) - paddingLeft - paddingRight,
+      (portrait ? width : height) - paddingTop - paddingBottom
+    );
+    ctx.restore();
+
+    rotate();
+    ctx.textAlign = "center";
+    ctx.fillStyle = "#000";
+    ctx.font = "60px sans-serif";
+    ctx.fillText("Inkplate 6", 0, 0);
+    ctx.restore();
+  }
 
   let snackbar = {
     show: false,
@@ -92,6 +165,16 @@
   <Button color="secondary" on:click={initSettings}>Reset</Button>
 </div>
 
+<canvas bind:this={canvas} width={500} height={500} />
+
 <Snackbar color={snackbar.color} bind:value={snackbar.show}>
   <div>{snackbar.text}</div>
 </Snackbar>
+
+<style>
+  canvas {
+    max-width: 100%;
+    margin: 1rem auto;
+    border: 2px solid gray;
+  }
+</style>
