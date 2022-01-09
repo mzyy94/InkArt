58e03f9353a6123b44c14a8ae9c097f6561aec3d
Yuki MIZUNO <mizuyuu0904@gmail.com>
2022-01-09 19:28:38 +0900

    Change firmware IDE to platform.io

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -12,6 +12,10 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+#include "inkplate.hpp"
+
+Inkplate display(DisplayMode::INKPLATE_3BIT);
+
 void main_task(void *)
 {
   std::cout << "Hello world!" << std::endl;
@@ -28,10 +32,19 @@ void main_task(void *)
   std::cout << spi_flash_get_chip_size() / (1024 * 1024) << "MB "
             << ((chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external") << " flash" << std::endl;
 
-  for (int i = 10; i >= 0; i--)
+  display.begin();
+  display.clearDisplay();
+  const auto width = display.width() / 8;
+  for (size_t i = 0; i < 8; i++)
   {
-    std::cout << "Restarting in " << i << " seconds..." << std::endl;
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    display.fillRect(width * i, 0, width, display.height(), i);
+  }
+
+  display.display();
+
+  for (;;)
+  {
+    vTaskDelay(1000000 / portTICK_PERIOD_MS);
   }
   std::cout << "Restarting now." << std::endl;
   vTaskDelay(1000 / portTICK_PERIOD_MS);
