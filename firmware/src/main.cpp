57c15c6e34735e3ae1190806f2f48d5cd7408f1b
Yuki MIZUNO <mizuyuu0904@gmail.com>
2022-01-09 23:33:12 +0900

    Split webapp part

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,11 +1,3 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -15,15 +7,10 @@
 #include <cstring>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "nvs_flash.h"
-#include "mdns.h"
-#include "lwip/apps/netbiosns.h"
-#include "esp_netif.h"
-#include "esp_wifi.h"
-#include "esp_event.h"
-#include "esp_log.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+
+#include "webapp.hpp"
 
 static const char *TAG = "main";
 
@@ -96,73 +83,11 @@ void main_task(void *)
   esp_restart();
 }
 
-static void init_localdomain(void)
-{
-  mdns_init();
-  mdns_hostname_set("inkart");
-  mdns_instance_name_set("InkArt web app");
-
-  mdns_txt_item_t txt_items[] = {{"path", "/"}};
-
-  ESP_ERROR_CHECK(mdns_service_add("ESP32-WebServer", "_http", "_tcp", 80, txt_items, sizeof(txt_items) / sizeof(txt_items[0])));
-
-  netbiosns_init();
-  netbiosns_set_name("inkart");
-}
-
-static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
-{
-  if (event_id == WIFI_EVENT_AP_STACONNECTED)
-  {
-    wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *)event_data;
-    ESP_LOGI(TAG, "station " MACSTR " join, AID=%d", MAC2STR(event->mac), event->aid);
-  }
-  else if (event_id == WIFI_EVENT_AP_STADISCONNECTED)
-  {
-    wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t *)event_data;
-    ESP_LOGI(TAG, "station " MACSTR " leave, AID=%d", MAC2STR(event->mac), event->aid);
-  }
-}
-
-void init_ap()
-{
-  ESP_ERROR_CHECK(esp_netif_init());
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
-  esp_netif_create_default_wifi_ap();
-
-  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
-
-  uint8_t macaddr[6];
-  esp_read_mac(macaddr, ESP_MAC_WIFI_SOFTAP);
-
-  wifi_config_t wifi_config = {};
-  snprintf(reinterpret_cast<char *>(wifi_config.ap.ssid), 12, "InkArt%02x%02x", macaddr[2], macaddr[3]);
-  snprintf(reinterpret_cast<char *>(wifi_config.ap.password), 12, "iNKaRT%02x%02x", macaddr[4], macaddr[5]);
-  wifi_config.ap.channel = 13;
-  wifi_config.ap.max_connection = 4;
-  wifi_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
-
-  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
-  ESP_ERROR_CHECK(esp_wifi_start());
-
-  ESP_LOGI(TAG, "SSID:%s password:%s", wifi_config.ap.ssid, wifi_config.ap.password);
-}
-
 extern "C"
 {
   void app_main()
   {
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+    init_nvs();
 
     init_ap();
     init_localdomain();
---
+++ firmware/src/webapp.hpp
@@ -0,0 +1,4 @@
+#pragma once
+void init_nvs();
+void init_localdomain(void);
+void init_ap();
