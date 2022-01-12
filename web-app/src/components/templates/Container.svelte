247dad6f1d025eda9c5ad3238732dee53eeabc5e
Yuki MIZUNO <mizuyuu0904@gmail.com>
2022-01-13 01:46:16 +0900

    Add home button

--- web-app/src/components/templates/Container.svelte
+++ web-app/src/components/templates/Container.svelte
@@ -1,4 +1,13 @@
+<script>
+  import { Button } from "smelte";
+
+  function toHome() {
+    location.hash = "#";
+  }
+</script>
+
 <h1 class="text-center md:text-4xl text-2xl">
+  <Button color="primary" icon="home" text light flat on:click={toHome} />
   <slot name="title">Title</slot>
 </h1>
 <section class="md:w-2/3 sm:w-auto md:m-auto m-3">
