7810660e06d6259109806211f06eeebb942ecd86
Yuki MIZUNO <mizuyuu0904@gmail.com>
2022-01-30 03:31:06 +0900

    Remove unused components

--- web-app/src/components/atoms/ActionIcon.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import { Icon } from "smelte";
-  export let icon: string;
-  export let callback: Function;
-  let className = "";
-	export { className as class };
-</script>
-<Icon on:click={callback()} class={`cursor-pointer ${className}`}>{icon}</Icon>
