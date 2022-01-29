e0b21ff3d3e89e0ada44e44dda9113236627ccb5
Yuki MIZUNO <mizuyuu0904@gmail.com>
2022-01-30 02:51:43 +0900

    Remove smelte from PhotoGrid component

--- web-app/src/components/molecules/PhotoGrid.svelte
+++ web-app/src/components/molecules/PhotoGrid.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { ProgressLinear } from "smelte";
   import PhotoCard from "./PhotoCard.svelte";
   import type { Entry } from "../../api";
 
@@ -7,13 +6,23 @@
   export let loading: boolean;
 </script>
 
-<section class="w-full">
-  {#if loading}
-    <ProgressLinear />
-  {/if}
-  <div class="grid md:grid-cols-2 grid-cols-1">
+<section aria-busy={loading}>
+  <div>
     {#each data as elm}
       <PhotoCard data={elm} on:delete on:hide />
     {/each}
   </div>
 </section>
+
+<style>
+  div {
+    display: grid;
+    column-gap: 20px;
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+  @media (min-width: 768px) {
+    div {
+      grid-template-columns: repeat(2, minmax(0, 1fr));
+    }
+  }
+</style>
