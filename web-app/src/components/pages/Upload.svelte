002f23c7faf8eb7e5a5c69fc133552676cacb83b
Yuki MIZUNO <mizuyuu0904@gmail.com>
2022-01-30 01:39:19 +0900

    Remove smelte from Upload component

--- web-app/src/components/pages/Upload.svelte
+++ web-app/src/components/pages/Upload.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-  import { Select, Slider } from "smelte";
   import Container from "../templates/Container.svelte";
   import ImageLoader from "../atoms/ImageLoader.svelte";
   import Grayscale from "../atoms/Grayscale.svelte";
   import Move from "../atoms/Move.svelte";
-  import ProgressButton from "../atoms/ProgressButton.svelte";
   import Snackbar from "../atoms/Snackbar.svelte";
 
   const modes = [
@@ -63,7 +61,12 @@
   <Container>
     <span slot="title">Upload Photo</span>
     <ImageLoader {img} />
-    <Select {label} items={modes} bind:value={mode} />
+    <p>{label}</p>
+    <select bind:value={mode}>
+      {#each modes as { value, text }}
+        <option {value}>{text}</option>
+      {/each}
+    </select>
     <span>Image size: {width}x{height}</span>
     <Move let:offsetX let:offsetY reset={mode} {active}>
       <Grayscale
@@ -79,26 +82,21 @@
       />
     </Move>
 
-    <fieldset class="my-3">
-      <p class="text-gray-700">Brightness: {brightness.toFixed(2)}</p>
-      <Slider
+    <fieldset>
+      <p>Brightness: {brightness.toFixed(2)}</p>
+      <input
+        type="range"
         min={0}
         max={3}
-        default={1}
         step={0.05}
         disabled={!active}
         bind:value={brightness}
       />
     </fieldset>
 
-    <ProgressButton
-      on:click={uploadImage}
-      disabled={!active}
-      loading={uploading}
-    >
-      Upload
-      <span slot="loading">Uploading...</span>
-    </ProgressButton>
+    <button on:click={uploadImage} disabled={!active} aria-busy={uploading}>
+      {uploading ? "Uploading..." : "Upload"}
+    </button>
   </Container>
 </main>
 
