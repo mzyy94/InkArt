9fa8efbd02dc7152c926cbfc3edddc427caa8d3a
Yuki MIZUNO <mizuyuu0904@gmail.com>
2022-01-30 02:17:55 +0900

    Remove smelte from InfoLabel component

--- web-app/src/components/atoms/InfoLabel.svelte
+++ web-app/src/components/atoms/InfoLabel.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
-  import { Icon } from "smelte";
   export let icon: string;
   export let title: string;
 </script>
 
-<section class="flex justify-between border-gray-500 border-t p-4">
-  <span class="text-l flex text-gray-700 font-bold">
-    <Icon class="text-l align-text-bottom self-center mr-2">{icon}</Icon>
+<section>
+  <span>
+    <i class="material-icons">{icon}</i>
     {title}:
   </span>
-  <span class="align-text-bottom self-center text-gray-800">
+  <span>
     <slot />
   </span>
 </section>
+
+<style>
+  section {
+    width: 100%;
+    height: 1em;
+    vertical-align: middle;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+  i {
+    vertical-align: top;
+  }
+</style>
