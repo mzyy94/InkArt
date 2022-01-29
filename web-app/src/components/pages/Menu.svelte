569485102a076e02b9f4599477642e167f5f8418
Yuki MIZUNO <mizuyuu0904@gmail.com>
2022-01-30 03:29:14 +0900

    Remove smelte from Menu component

--- web-app/src/components/pages/Menu.svelte
+++ web-app/src/components/pages/Menu.svelte
@@ -1,48 +1,50 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { push } from "svelte-spa-router";
-  import { List, Dialog, Button } from "smelte";
   import Container from "../templates/Container.svelte";
   import api from "../../api";
 
   const menuItems = [
     {
       id: "upload",
-      value: "/upload",
+      value: "#/upload",
       text: "Upload photo",
       icon: "photo",
       subheading: "Edit and upload photo",
     },
     {
       id: "files",
-      value: "/files",
+      value: "#/files",
       text: "File Management",
       icon: "folder",
       subheading: "Organize photos to be shown",
     },
     {
       id: "control",
-      value: "/control",
+      value: "#/control",
       text: "Display Control",
       icon: "gamepad",
       subheading: "Set display orientation and paddings",
     },
     {
       id: "settings",
-      value: "/settings",
+      value: "#/settings",
       text: "Settings",
       icon: "settings",
       subheading: "Change device Date/Time, refresh interval, etc.",
     },
     {
       id: "info",
-      value: "/info",
+      value: "#/info",
       text: "Information",
       icon: "info",
       subheading: "App version and device information",
     },
     {
       id: "power",
+      value: "#",
+      action: () => {
+        confirmReboot = true;
+      },
       text: "Power",
       icon: "power_settings_new",
       subheading: "Reboot and enter photo display mode",
@@ -65,14 +67,6 @@
   });
 
   let confirmReboot = false;
-  function selectListItem(e: { detail: string }) {
-    const menuItem = menuItems.find(({ id }) => id == e.detail);
-    if (menuItem?.value) {
-      push(menuItem.value);
-    } else if (menuItem?.id == "power") {
-      confirmReboot = true;
-    }
-  }
 
   function reboot() {
     fetch("/api/v1/system/reboot", { method: "POST" }).then(() => {
@@ -84,17 +78,47 @@
 <main>
   <Container>
     <span slot="title">InkArt</span>
-    <section>
-      <List on:change={selectListItem} items={menuItems} class="w-full" />
+    <section class="container">
+      {#each menuItems as item}
+        <a href={item.value} on:click={item.action}>
+          <i class="material-icons">{item.icon}</i>
+          <div>
+            <h4>{item.text}</h4>
+            <p>{item.subheading}</p>
+          </div>
+        </a>
+      {/each}
     </section>
   </Container>
 </main>
 
-<Dialog value={confirmReboot}>
-  <h5 slot="title">Reboot</h5>
-  <div class="text-gray-700">Do you want to leave setup mode?</div>
-  <div slot="actions">
-    <Button text on:click={() => (confirmReboot = false)}>Cancel</Button>
-    <Button text on:click={reboot} color="error">Reboot</Button>
-  </div>
-</Dialog>
+<dialog open={confirmReboot}>
+  <article>
+    <header>Reboot</header>
+    <p>Do you want to leave setup mode?</p>
+    <footer>
+      <button class="secondary" on:click={() => (confirmReboot = false)}>
+        Cancel
+      </button>
+      <button class="contrast" on:click={reboot}>Reboot</button>
+    </footer>
+  </article>
+</dialog>
+
+<style>
+  section > a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+  }
+  section > a:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+  section > a > i {
+    width: 1em;
+    margin: 1em;
+  }
+  section > a > div > * {
+    margin: 0;
+  }
+</style>
